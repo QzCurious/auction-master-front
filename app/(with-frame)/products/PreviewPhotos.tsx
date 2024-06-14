a9c7b4e5651b41f5f90dc9b744ff1416fc4a38cd
Qz <qzcurious@gmail.com>
2024-06-14 16:39:39 +0800

    product photos carousel

--- app/(with-frame)/products/PreviewPhotos.tsx
+++ app/(with-frame)/products/PreviewPhotos.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
 import clsx from 'clsx'
-import { useState } from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import { useEffect, useState } from 'react'
 
 export default function PreviewPhotos({
   photos,
@@ -12,33 +13,55 @@ export default function PreviewPhotos({
     index: number
   }>
 }) {
-  const [i, setI] = useState(0)
+  const [emblaRef, emblaApi] = useEmblaCarousel({ startIndex: 0 })
+  const [i, setI] = useState(emblaApi?.selectedScrollSnap() ?? 0)
+
+  useEffect(() => {
+    if (emblaApi) {
+      emblaApi.on('select', ({ selectedScrollSnap }) => setI(selectedScrollSnap()))
+    }
+  }, [emblaApi])
 
   return (
-    <div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'>
-      <img
-        src={photos[i]?.photo ?? ''}
-        className='h-full w-full object-cover object-center'
-        alt=''
-      />
+    <div
+      ref={emblaRef}
+      className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75'
+    >
+      <div className='flex'>
+        {photos.map(({ photo, index }) => (
+          <div key={index} className='min-w-0 shrink-0 basis-full'>
+            <img
+              src={photo}
+              className='h-full w-full object-cover object-center'
+              alt=''
+            />
+          </div>
+        ))}
+      </div>
 
       {photos.length > 1 && (
-        <div className='absolute bottom-0 left-0 right-0 top-auto flex h-fit justify-end gap-x-3 px-1.5 pb-1.5'>
+        <div className='absolute bottom-0 left-0 right-0 top-0 flex justify-between'>
           <button
             type='button'
-            className={clsx('size-7', i === 0 && 'opacity-50')}
-            onClick={() => setI(Math.max(0, i - 1))}
+            className={clsx(
+              'h-full w-20 bg-gradient-to-r from-black/60 to-black/0 opacity-50 transition-opacity hover:opacity-100',
+              i === 0 && 'pointer-events-none !opacity-0',
+            )}
+            onClick={() => emblaApi?.scrollPrev()}
           >
-            <span className='sr-only'>Move left</span>
-            <ArrowLeftIcon className='rounded-full bg-white/80 stroke-2 p-1 text-gray-400 hover:bg-white hover:text-gray-600' />
+            <span className='sr-only'>Move to previous</span>
+            <ChevronLeftIcon className='mr-auto size-12 stroke-2 p-1 text-white' />
           </button>
           <button
             type='button'
-            className={clsx('size-7', i === photos.length - 1 && 'opacity-50')}
-            onClick={() => setI(Math.min(photos.length - 1, i + 1))}
+            className={clsx(
+              'h-full w-20 bg-gradient-to-l from-black/60 to-black/0 opacity-50 transition-opacity hover:opacity-100',
+              i === photos.length - 1 && 'pointer-events-none !opacity-0',
+            )}
+            onClick={() => emblaApi?.scrollNext()}
           >
-            <span className='sr-only'>Move right</span>
-            <ArrowRightIcon className='rounded-full bg-white/80 stroke-2 p-1 text-gray-400 hover:bg-white hover:text-gray-600' />
+            <span className='sr-only'>Move to next</span>
+            <ChevronRightIcon className='ml-auto size-12 stroke-2 p-1 text-white' />
           </button>
         </div>
       )}
