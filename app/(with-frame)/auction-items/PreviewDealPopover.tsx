f26fb197884dce4efb71e7c106beabce6eb295e4
Qz <qzcurious@gmail.com>
2024-09-06 15:24:16 +0800

    PreviewDealPopover style

--- app/(with-frame)/auction-items/PreviewDealPopover.tsx
+++ app/(with-frame)/auction-items/PreviewDealPopover.tsx
@@ -22,7 +22,7 @@ export default function PreviewDealPopover({
         anchor={{ to: 'top end', gap: 2 }}
         transition
         className={clsx(
-          'rounded border border-black/10 bg-white px-2 py-1.5 shadow-md transition duration-200 ease-in-out [--anchor-gap:var(--spacing-5)] data-[closed]:translate-y-1 data-[closed]:opacity-0',
+          'z-30 rounded border border-black/10 bg-white px-2 py-1.5 shadow-md transition duration-200 ease-in-out [--anchor-gap:var(--spacing-5)] data-[closed]:translate-y-1 data-[closed]:opacity-0',
         )}
       >
         <PreviewDeal auctionItemId={auctionItemId} />
@@ -43,6 +43,13 @@ function PreviewDeal({ auctionItemId }: { auctionItemId: AuctionItem['id'] }) {
     <table>
       <tbody>
         <tr>
+          <td>結算金額</td>
+          <td className='pl-4 text-end'>
+            {currencySign('JPY')}
+            {data.data.price.toLocaleString()}
+          </td>
+        </tr>
+        <tr>
           <td>日拍手續費</td>
           <td className='pl-4 text-end text-rose-600'>
             {currencySign('JPY')}
@@ -50,10 +57,31 @@ function PreviewDeal({ auctionItemId }: { auctionItemId: AuctionItem['id'] }) {
           </td>
         </tr>
         <tr>
-          <td>佣金</td>
+          <td>平台手續費</td>
           <td className='pl-4 text-end text-rose-600'>
             {currencySign('JPY')}
             {data.data.commission.toLocaleString()}
+          </td>
+        </tr>
+        <tr>
+          <td>紅利</td>
+          <td className='pl-4 text-end'>
+            {data.data.commissionBonus.toLocaleString()}
+          </td>
+        </tr>
+
+        <tr>
+          <td className='h-2' colSpan={2}>
+            <div className='flex h-full items-center'>
+              <div className='w-full border-t border-zinc-300'></div>
+            </div>
+          </td>
+        </tr>
+
+        <tr>
+          <td>大師幣</td>
+          <td className='pl-4 text-end'>
+            {data.data.price - data.data.yahooAuctionFee - data.data.commission}
           </td>
         </tr>
         <tr>
