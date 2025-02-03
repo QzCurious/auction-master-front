ce3bde91b2c581aafe88a83a17413a7825987472
Qz <qzcurious@gmail.com>
2025-02-03 22:05:24 +0800

    default site title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
+import { SITE_NAME } from '@/domain/static/static'
 import '@fontsource-variable/noto-sans-tc'
 import { Noto_Sans_TC } from 'next/font/google'
 import './globals.css'
-import ToastContainer from './ToastContainer'
 import Providers from './Providers'
+import ToastContainer from './ToastContainer'
 
 const font = Noto_Sans_TC({ subsets: [], weight: 'variable' })
+
+export const metadata = {
+  title: SITE_NAME,
+}
 
 export default async function RootLayout({
   children,
